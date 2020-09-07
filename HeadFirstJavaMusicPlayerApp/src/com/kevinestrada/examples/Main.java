b5ad15e8d922634f56708b93be13ff0ee9586cd5
Kevin Estrada <ke81297@gmail.com>
2020-09-07 02:17:50 -0500

    first commit

---
+++ HeadFirstJavaMusicPlayerApp/src/com/kevinestrada/examples/Main.java
@@ -0,0 +1,9 @@
+package com.kevinestrada.examples;
+
+public class Main {
+
+    public static void main(String[] args) {
+	MinniMiniMusicApp mini = new MinniMiniMusicApp();
+	mini.play();
+    }
+}
